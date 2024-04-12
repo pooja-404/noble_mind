2f5671f5cc1cf930b9197b48c419206f0adad64b
pooja-404 <pk3924792@gmail.com>
2024-04-12 12:34:32 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <div className="overflow-hidden">
 
-      <Head>
+      {/* <Head>
         <title>Noble-mind</title>
         <meta name="title" content="Noble-mind" />
         <meta name="description" content="Inspiring Innovation, Elevating Solutions – Noble Mind" />
@@ -52,7 +52,15 @@ export default function Home() {
         <meta property="twitter:title" content="Noble-mind" />
         <meta property="twitter:description" content="Inspiring Innovation, Elevating Solutions – Noble Mind" />
         <meta property="twitter:image" content="https://images.unsplash.com/photo-1712229218439-befac221f1f1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      </Head> */}
+
+      <Head>
+        <title>Noble-Mind</title>
+        <meta name="description" content="Your page description" />
+        <h1>Your Page Content</h1>
+        <Image src='/assets/images/meta.webp' alt="Your Image Alt Text" width={1440} height={600} />
       </Head>
+
       {data ? (
         <div>{<Preloder />}</div>
       ) : (
