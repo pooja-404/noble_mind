ceb827c26ac431ad6f0bcf31280a3c226e65da1a
pooja-404 <pk3924792@gmail.com>
2024-04-16 20:46:57 +0530

    commit

--- src/app/components/Newslattter.jsx
+++ src/app/components/Newslattter.jsx
@@ -4,7 +4,7 @@ export default function Newslatter()
 {
     return (
         <div className="relative z-1 max-lg:mt-[-86px] px-3" id="blogs">
-            <div className="max-w-[1140px] container mx-auto px-3 bg-bg_image2 md:h-[318px] h-full py-7 md:py-0 rounded-[20px] flex  justify-center flex-col translate-y-20">
+            <div className="max-w-[1140px] container 2xl:max-w-[1320px] mx-auto px-3 bg-bg_image2 md:h-[318px] bg-no-repeat bg-cover   h-full py-7 md:py-0 rounded-[20px] flex  justify-center flex-col translate-y-20">
                 <h2 className='text-[#FDFDFF] font-Outfit  lg:text-[48px]  sm:text-5xl text-3xl font-light md:leading-[57.6px] text-center' data-aos="zoom-in-down">Subscribe to our <span className="font-semibold"> Newsletter!</span></h2>
                 <p className="font-Exo sm:text-base text-sm font-normal md:leading-[24px] text-[#FDFDFF] text-center pt-2" data-aos="zoom-in-down">Be the first to get exclusive offers and the latest news.</p>
                 <div className='sm:pl-5 pl-2 pr-2 sm:pr-3 box-border py-2 bg-[#FFFFFF0D] rounded-[6px] border border-[#FDFDFF33] mx-auto md:w-[448px]  w-full flex justify-between items-center mt-5 lg:mt-10 ' data-aos="zoom-in-down">
